02c43058d593ccbc19d43ce079a7c65cd0ce9deb
__JM_Joy__ <918734043@qq.com>
2019-12-12 13:03:15 +0800

    Remove all warning abi.

---
+++ phper-sys/php_wrapper.h
@@ -0,0 +1,10 @@
+//
+// Created by jmjoy on 19-12-12.
+//
+
+#ifndef PHPER_PHP_WRAPPER_H
+#define PHPER_PHP_WRAPPER_H
+
+#include <php.h>
+
+#endif //PHPER_PHP_WRAPPER_H
--- phper-sys/src/macro.rs
+++ phper-sys/src/macro.rs
@@ -1,26 +1,13 @@
 #[macro_export]
 macro_rules! emalloc {
     ( $( $x:expr ),* ) => {
-        crate::_emalloc( $( $x, )* )
+        $crate::_emalloc( $( $x, )* )
     };
 }
 
 #[macro_export]
 macro_rules! efree {
     ( $( $x:expr ),* ) => {
-        crate::_efree( $( $x, )* )
+        $crate::_efree( $( $x, )* )
     };
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_emalloc_efree() {
-        unsafe {
-            let ptr = emalloc!(1);
-            efree!(ptr);
-        }
-    }
 }
