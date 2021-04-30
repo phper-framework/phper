e69a4b57a659474203a829334666c402468a74e7
jmjoy <918734043@qq.com>
2021-04-30 19:23:42 +0800

    Add integration test crate.

---
+++ tests/integration/tests/integration.rs
@@ -0,0 +1,5 @@
+use phper_test::test_php_scripts;
+use std::{env, path::Path};
+
+#[test]
+fn test_php() {}
---
+++ tests/integration/tests/php/values.php
@@ -0,0 +1,5 @@
+<?php
+
+ini_set("display_errors", "On");
+ini_set("display_startup_errors", "On");
+error_reporting(E_ALL);
