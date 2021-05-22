dc75ec712097a2996417ea23abfc9de88213b913
jmjoy <918734043@qq.com>
2021-05-22 10:32:31 +0800

    Re-add php version 7.0.

--- tests/integration/tests/php/classes.php
+++ tests/integration/tests/php/classes.php
@@ -2,7 +2,13 @@
 
 require_once __DIR__ . '/_common.php';
 
-assert_throw(function () { new \IntegrationTest\A(); }, "ArgumentCountError", 0, "IntegrationTest\\A::__construct(): expects at least 2 parameter(s), 0 given");
+if (PHP_VERSION_ID >= 70100) {
+    $argumentCountErrorName = "ArgumentCountError";
+} else {
+    $argumentCountErrorName = "TypeError";
+}
+
+assert_throw(function () { new \IntegrationTest\A(); }, $argumentCountErrorName, 0, "IntegrationTest\\A::__construct(): expects at least 2 parameter(s), 0 given");
 
 $a = new \IntegrationTest\A("foo", 99);
 assert_eq($a->speak(), "name: foo, number: 99");
