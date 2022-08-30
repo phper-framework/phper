45b1daa2081f436bcc122e608e65f4c9f1793949
jmjoy <jmjoy@apache.org>
2022-08-30 20:10:59 +0800

    Update license headers.

--- examples/hello/tests/php/test.php
+++ examples/hello/tests/php/test.php
@@ -1,6 +1,6 @@
 <?php
 
-// Copyright (c) 2019 jmjoy
+// Copyright (c) 2022 PHPER Framework Team
 // PHPER is licensed under Mulan PSL v2.
 // You can use this software according to the terms and conditions of the Mulan
 // PSL v2. You may obtain a copy of Mulan PSL v2 at:
