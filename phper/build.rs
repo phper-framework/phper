5265f6a52fe1f62cce7801afe8f3177e6d0861a4
jmjoy <jmjoy@apache.org>
2022-09-06 09:07:41 +0800

    Macsos link args.

--- phper/build.rs
+++ phper/build.rs
@@ -17,6 +17,8 @@ fn main() {
     {
         println!("cargo:rustc-cdylib-link-arg=-undefined");
         println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
+        println!("cargo:rustc-link-arg=-undefined");
+        println!("cargo:rustc-link-arg=dynamic_lookup");
     }
 
     assert_eq!(
