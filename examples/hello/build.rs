8f58b49e87afb6792b99b4c0c5fbf5020b2497bc
jmjoy <jmjoy@apache.org>
2022-12-03 12:40:53 +0800

    Add simple example hello. (#76)

---
+++ examples/hello/build.rs
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 PHPER Framework Team
+// PHPER is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+fn main() {
+    #[cfg(target_os = "macos")]
+    {
+        println!("cargo:rustc-link-arg=-undefined");
+        println!("cargo:rustc-link-arg=dynamic_lookup");
+    }
+}
---
+++ examples/hello/src/lib.rs
@@ -0,0 +1,40 @@
+// Copyright (c) 2022 PHPER Framework Team
+// PHPER is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+use phper::{echo, functions::Argument, modules::Module, php_get_module, values::ZVal};
+
+/// The php function, receive arguments with type `ZVal`.
+fn say_hello(arguments: &mut [ZVal]) -> phper::Result<()> {
+    // Get the first argument, expect the type `ZStr`, and convert to Rust utf-8
+    // str.
+    let name = arguments[0].expect_z_str()?.to_str()?;
+
+    // Macro which do php internal `echo`.
+    echo!("Hello, {}!\n", name);
+
+    Ok(())
+}
+
+/// This is the entry of php extension, the attribute macro `php_get_module`
+/// will generate the `extern "C" fn`.
+#[php_get_module]
+pub fn get_module() -> Module {
+    // New `Module` with extension info.
+    let mut module = Module::new(
+        env!("CARGO_CRATE_NAME"),
+        env!("CARGO_PKG_VERSION"),
+        env!("CARGO_PKG_AUTHORS"),
+    );
+
+    // Register function `say_hello`, with one argument `name`.
+    module.add_function("say_hello", say_hello, vec![Argument::by_val("name")]);
+
+    module
+}
