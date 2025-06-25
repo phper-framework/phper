718ecc304afc57fbcd0441555e8ff205c42a3115
jmjoy <jmjoy@apache.org>
2025-06-25 18:14:35 +0800

    fix: Update copyright notice and licensing information in log.rs

--- phper-test/src/log.rs
+++ phper-test/src/log.rs
@@ -1,3 +1,13 @@
+// Copyright (c) 2025 PHPER Framework Team
+// PHPER is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan
+// PSL v2. You may obtain a copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
 //! Logging utilities for phper tests.
 //!
 //! This module provides a centralized logging setup specifically designed for
